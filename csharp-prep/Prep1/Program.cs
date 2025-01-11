a14b786a7bdf3ce7a5f023dfba03d738e73455db
daildubath <daildubath@byui.edu>
2025-01-11 16:55:36 -0700

    did prep 1 and 2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Get the strings from the user
+        Console.Write("What is your first name? ");
+        string FirstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string LastName = Console.ReadLine();
+
+        // Console.Write("Your name is ");
+        // Console.Write(LastName);
+        // Console.Write(", ");
+        // Console.Write(FirstName);
+        // Console.Write(" ");
+        // Console.Write(LastName);
+        // Console.WriteLine(".");
+
+        Console.WriteLine($"Your name is {LastName}, {FirstName} {LastName}.");
+
     }
 }
+
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string UserGrade = Console.ReadLine();
+
+        int grade = int.Parse(UserGrade);
+
+        string LetterGrade = "";
+
+        if (grade >= 90)
+        {
+            LetterGrade = "A";
+        }
+        else if (grade >= 80)
+        {
+            LetterGrade = "B";
+        }
+        else if (grade >= 70)
+        {
+            LetterGrade = "C";
+        }
+        else if (grade >= 60)
+        {
+            LetterGrade = "D";
+        }
+        else
+        {
+            LetterGrade = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is an {LetterGrade}.");
+        if (grade >= 70)
+        {
+            Console.WriteLine("You will pass the course. Congradulations!");
+        }
+        else 
+        {
+            Console.WriteLine("You will fail the course. ");
+        }
+
+
+
+
     }
 }

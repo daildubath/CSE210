e450d48ea8cf46879aef1fb0c5f1183d6216beb8
daildubath <ben23050@byui.edu>
2025-03-15 18:47:16 -0600

    Like and Comment! We sell cheetos

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,6 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        System.Console.WriteLine("Good day to you too, potato head.");
     }
 }
